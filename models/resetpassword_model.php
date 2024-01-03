0317183c64f5dd03da195e5df5d43c5290fd4bae
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-03 15:47:59 +0100

    forget password

---
+++ models/resetpassword_model.php
@@ -0,0 +1,50 @@
+<?php
+if(isset($_POST['reset-request-submit'])){
+    $selector = bin2hex(random_bytes(8));
+    $token = random_bytes(32);
+
+    $url = "http://localhost/Hafsa_Moatassim_Billah_Alpha/index.php?page=newpassword&&selector=" . $selector . "&validator=" . bin2hex($token);
+
+    $expires = date("U") + 1800;
+    
+    extract($_POST);
+    
+    $sql = "DELETE FROM pwdReset WHERE pwdResetEmail=?";
+        $stmt = mysqli_stmt_init($db);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            echo "there was an error";
+            exit();
+        }else{
+            mysqli_stmt_bind_param($stmt, "s", $userEmail);
+            mysqli_stmt_execute($stmt);
+            }
+
+    $sql = "INSERT INTO pwdReset (pwdResetEmail, pwdResetSelector, pwdResetToken, pwdResetExpires) VALUES (?, ?, ?, ?)";
+        $stmt = mysqli_stmt_init($db);
+        if(!mysqli_stmt_prepare($stmt,$sql)){
+            echo "there was an error";
+            exit();
+        }else{
+            $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+            mysqli_stmt_bind_param($stmt, "ssss", $userEmail ,$selector, $hashedToken, $expires);
+            mysqli_stmt_execute($stmt); 
+            }
+
+            mysqli_stmt_close($stmt);
+            mysqli_close();
+
+            $to = $userEmail;
+            $subject = "Reset your password";
+            $message = '<p>We receive a password reset request. The link to reset your password is below if you did not make this request, you can ignore this email.</p>';
+            $message .= '<p>Here is your password reset link: </br>';
+            $message .= '<a href="' . $url . '">' . $url . '</a></p>';
+
+            $headers = "From: XENO <youcode270@gmail.com>";
+            $headers .="";
+
+            
+        }
+        // else{
+        //  header("location:index.php?page=home");
+        // }
+?>
--- views/signup_view.php
+++ views/signup_view.php
@@ -6,7 +6,7 @@
               <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="index.php?page=signup" method="post" enctype="multipart/form-data">
+              <form class="space-y-4 md:space-y-6" action="index.php?page=signup" method="POST" enctype="multipart/form-data">
                   <div>
                       <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
                       <input type="text" name="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
@@ -24,7 +24,7 @@
                     <input type="file" name="pp" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" >
 
                   <div class="flex items-center justify-between">
-                      <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      <a href="index.php?page=resetpassword" class="text-sm font-medium text-blue-600 hover:underline dark:text-primary-500">Forgot password?</a>
                   </div>
                   <button name="submit" class="w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400">
