fc8f4ff78880a7133ddc9d32754d549795c3f0c8
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 17:57:43 +0100

    fix the table

--- models/users_model.php
+++ models/users_model.php
@@ -3,9 +3,10 @@
 $api_url = "https://jsonplaceholder.typicode.com/users";
 $users_json = file_get_contents($api_url);
 $users = json_decode($users_json, true);
+?>
 
-foreach ($users as $user) {
-echo '<div class="flex flex-col">
+
+ <div class="flex flex-col">
   <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
     <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
       <div class="overflow-hidden">
@@ -18,25 +19,32 @@ echo '<div class="flex flex-col">
               <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                 Email
               </th>
+              <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+              Operation 
+              </th>
             </tr>
           </thead>
+          <?php foreach ($users as $user) { ?>
           <tbody>
             <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-              '.$user['username'].'
+              <?php  echo $user['username'] ?>
               </td>
               <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-              '.$user['email'].'
+              <?php  echo $user['email'] ?>
               </td>
+                <td style:>
+                    <a href="#" class="bg-blue-500 p-2 text-white hover:shadow-lg text-xs font-thin">Edit</a>
+                    <a href="#" class="bg-red-500 p-2 text-white hover:shadow-lg text-xs font-thin">Remove</a>
+                </td>
     
             </tr>
-          </tbody>
+          </tbody> 
+          <?php } ?>
         </table>
       </div>
     </div>
   </div>
 </div>
-';
-}
 
-?>
+
