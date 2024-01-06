9b22de5f1c57ced43aec13d8ffe69304d3345ced
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 21:44:31 +0100

    class product

--- _classes/Product.php
+++ _classes/Product.php
@@ -1,12 +1,45 @@
  <?php
 
-//  class user
-//  {
-//     function edit(){
-//         global $db;
-//         $query = "UPDATE users SET username = ?, email = ?,  WHERE user_id = ?";
-//         $stm = $db->prepare($query);
-//         $stm->bind_param('ssssi', $this->username, $this->email);
-//         return $stm->execute();
-//     }
-//  }
+class ProductCRUD {
+    private $db;
+
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    public function addProduct($productName, $productDescription) {
+        $query = "INSERT INTO product (productName, productDescription) VALUES ('$productName', '$productDescription')";
+        return $this->db->query($query);
+    }
+    
+
+    public function getAllProducts() {
+        $query = "SELECT * FROM product";
+        $result = $this->db->query($query);
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getProductById($productId) {
+        $query = "SELECT * FROM product WHERE productID = $productId";
+        $result = $this->db->query($query);
+
+        return $result->fetch_assoc();
+    }
+
+    public function updateProduct($productId, $productName, $productDescription) {
+        $query = "UPDATE product SET productName = '$productName', productDescription = '$productDescription' WHERE productID = $productId";
+        return $this->db->query($query);
+    }
+
+    public function deleteProduct($productId) {
+        $query = "DELETE FROM product WHERE productID = $productId";
+        return $this->db->query($query);
+    }
+}
+
+?>
+
+
+
+    
