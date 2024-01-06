53079a1cb2b493b9e4f6628a860c2d8ce5c8188f
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 20:07:53 +0100

    update function

---
+++ _classes/Product.php
@@ -0,0 +1,12 @@
+ <?php
+
+//  class user
+//  {
+//     function edit(){
+//         global $db;
+//         $query = "UPDATE users SET username = ?, email = ?,  WHERE user_id = ?";
+//         $stm = $db->prepare($query);
+//         $stm->bind_param('ssssi', $this->username, $this->email);
+//         return $stm->execute();
+//     }
+//  }
