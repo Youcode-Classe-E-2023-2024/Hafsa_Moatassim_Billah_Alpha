a885a87d3f7af65fb649fefc94774137cbbd4bb3
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 20:07:22 +0100

    fixing the colors

--- models/produit_model.php
+++ models/produit_model.php
@@ -5,23 +5,43 @@ $products_json = file_get_contents($api_url);
 $products = json_decode($products_json, true);
 ?>
 
+<div class="w-full text-gray-700 bg-black-800 dark-mode:text-gray-200 dark-mode:bg-gray-800">
+  <div x-data="{ open: false }" class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
+    <div class="p-4 flex flex-row items-center justify-between">
+      <a href="#" class="text-lg font-semibold tracking-widest text-white-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">XENO  </a>
+      <button class="md:hidden rounded-lg focus:outline-none focus:shadow-outline" @click="open = !open">
+        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+          <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+          <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+        </svg>
+      </button>
+    </div>
+    <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow pb-4 md:pb-0 hidden md:flex md:justify-end md:flex-row">
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="index.php?page=home">Home</a> 
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="index.php?page=produit">Products</a>
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="index.php?page=addproduct">Add Product</a>
+      <a class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="index.php?page=logout">Logout</a>   
+    </nav>
+  </div>
+</div>
+
    <div class="table w-full p-2">
         <table class="w-full border">
             <thead>
                 <tr class="bg-gray-50 border-b">
-                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-950">
                         <div class="flex items-center justify-center">
                             Title
                             
                         </div>
                     </th>
-                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-950">
                         <div class="flex items-center justify-center">
                             Description
                             
                         </div>
                     </th>
-                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                    <th class="p-2 border-r cursor-pointer text-sm font-thin text-gray-950">
                         <div class="flex items-center justify-center">
                             Action
 
