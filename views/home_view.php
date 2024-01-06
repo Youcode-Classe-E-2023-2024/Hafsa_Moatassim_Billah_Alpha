4b1dd17475f285daf5ba916fa586a0a15221ee18
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 16:20:28 +0100

    fix the home page

--- views/home_view.php
+++ views/home_view.php
@@ -1,4 +1,4 @@
-<body class="back">
+<body  >
 <section>
     <div
         class="absolute inset-0 bg-white/75 sm:bg-transparent sm:from-white/95 sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
@@ -9,19 +9,19 @@
     >
         <div class="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
             <h1 class="text-3xl font-extrabold sm:text-5xl">
-                Welcome to Your Forever
+                Welcome to 
 
-                <strong class="block font-extrabold text-blue-950" >Rooms Chat!</strong>
+                <strong class="block font-extrabold text-blue-950">XENO</strong>
             </h1>
 
             <p class="mt-4 max-w-lg sm:text-xl/relaxed">
-                Let's find the perfect home for you, where great conversations await
+            Bienvenue dans l'univers fonctionnel de XENO ! 
             </p>
 
             <div class="mt-8 flex flex-wrap gap-4 text-center">
                 <a
                     href="index.php?page=login"
-                    class="block w-full rounded bg-blue-950 px-12 py-3 text-sm font-medium text-white shadow hover:bg-orange-700
+                    class="block w-full rounded bg-blue-950 px-12 py-3 text-sm font-medium text-white shadow hover:bg-green-700
                     focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
                 >
                     Get Started
@@ -29,9 +29,9 @@
 
                 <a
                     href="#"
-                    class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-gray-900 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+                    class="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-gray-900 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-black-500 sm:w-auto"
                 >
-                    Start chatting now!
+                    Start now!
                 </a>
             </div>
         </div>
