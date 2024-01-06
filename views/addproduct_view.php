ad7d7b97db1b92451ad6a25e9525ec9911ed5b8f
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 16:21:58 +0100

    fetch the data with api

---
+++ views/addproduct_view.php
@@ -0,0 +1,9 @@
+
+
+
+<form action="index.php?page=addproduct" method="post">
+    <label>Title: <input type="text" name="title"></label><br>
+    <label>Body: <textarea name="body"></textarea></label><br>
+    <label>User ID: <input type="text" name="userId"></label><br>
+    <input type="submit" name="submit" value="Add Product">
+</form>
