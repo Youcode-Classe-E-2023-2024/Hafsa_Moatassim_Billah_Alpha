25dd7c10e2bbe92d5276fa743c205012ca4c2836
HAFSA159 <hafsaelmoatassim99@gmail.com>
2024-01-06 21:45:05 +0100

    update

--- models/addproduct_model.php
+++ models/addproduct_model.php
@@ -1,23 +1,15 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $new_product_data = array(
-        'title' => $_POST['title'],
-        'body' => $_POST['body'],
-        'userId' => $_POST['userId']
-    );
 
-    $options = array(
-        'http' => array(
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => json_encode($new_product_data)
-        )
-    );
+// if (isset($_POST['submit'])) {
+//     $title = $_POST['title'];
+//     $description = $_POST['description'];
 
-    $context  = stream_context_create($options);
-    $api_url = "https://jsonplaceholder.typicode.com/posts";
-    $result = file_get_contents($api_url, false, $context);
 
-}
+//     $query = "INSERT INTO product (productName, productDescription) VALUES ('$productName', '$productDescription')";
+//     $result = $db->query($query);
+// }
+
+// $db->close();
+
 ?>
 
